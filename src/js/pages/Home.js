c02f72d729a5907ac7643b59eb06ec2558736b82
ericsantos <eric@exactaworks.com.br>
2020-10-20 12:01:52 -0300

    refactor: code review improvements

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,13 +1,10 @@
-import CartModel from '../models/Cart.js';
 import ProductsController from '../controllers/Products.js';
 import CartController from '../controllers/Cart.js';
 
 class Home {
   constructor() {
-    const cartModel = new CartModel();
-
     this.productsController = new ProductsController();
-    this.cartController = new CartController(cartModel);
+    this.cartController = new CartController();
   }
 }
 
