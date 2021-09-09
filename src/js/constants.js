360b70432e15d4ad4d43b2c5f0a425f708200439
ericsantos <eric@exactaworks.com.br>
2021-09-09 11:54:36 -0300

    refactor: storage util and constants

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,11 +1,5 @@
 const APP_STORAGE_KEY = '@bicyle-shop';
-const CART_STORAGE_KEY = `${APP_STORAGE_KEY}/cart`;
+const CART_STORAGE_KEY = 'cart';
+const API_BASE_URL = 'http://localhost:3000';
 
-const CUSTOM_EVENTS = {
-  CART_ADD: 'cart:add',
-  CART_INCREMENT: 'cart:increment',
-  CART_DECREMENT: 'cart:decrement',
-  CART_REMOVE: 'cart:remove',
-};
-
-export { APP_STORAGE_KEY, CART_STORAGE_KEY, CUSTOM_EVENTS };
+export { APP_STORAGE_KEY, CART_STORAGE_KEY, API_BASE_URL };
--- src/js/utils/storage.js
+++ src/js/utils/storage.js
@@ -1,14 +1,15 @@
 import { APP_STORAGE_KEY } from '../constants.js';
 
 const setStorageItem = (key, item) => {
-  localStorage.setItem(APP_STORAGE_KEY + key, JSON.stringify(item));
-};
+  localStorage.setItem(`${APP_STORAGE_KEY}/${key}`, JSON.stringify(item));
+}
 
-const getStorageItem = (key) =>
-  JSON.parse(localStorage.getItem(APP_STORAGE_KEY + key));
+const getStorageItem = (key) => {
+  return JSON.parse(localStorage.getItem(`${APP_STORAGE_KEY}/${key}`));
+}
 
 const removeStorageItem = (key) => {
-  localStorage.removeItem(APP_STORAGE_KEY + key);
-};
+  localStorage.removeItem(`${APP_STORAGE_KEY}/${key}`);
+}
 
 export { setStorageItem, getStorageItem, removeStorageItem };
